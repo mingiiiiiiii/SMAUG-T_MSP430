f35ba25ade4ea7889cc3cc8d9e2d54adca5ae2e8
mingiiiiiiii <mingi1684@kookmin.ac.kr>
2024-11-15 22:41:24 +0900

    ✨ Add SMAUG-T NTT code using AES_randombytes

---
+++ SMAUG-T_NTT/toomcook.h
@@ -0,0 +1,11 @@
+#ifndef SMAUG_TOOMCOOK_H
+#define SMAUG_TOOMCOOK_H
+
+#include <stdint.h>
+
+#include "parameters.h"
+
+// #define poly_mul_acc SMAUG_NAMESPACE(poly_mul_acc)
+void poly_mul_acc(const int16_t a[LWE_N], const int16_t b[LWE_N], int16_t res[LWE_N]);
+
+#endif	// SMAUG_TOOMCOOK_H
